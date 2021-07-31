dd428e068f5689bff9efb02104906dda9dcd6c4a
Shane F. Carr <shane.carr@wustl.edu>
2021-07-31 16:57:46 -0500

    Error message improvement

--- test.sh
+++ test.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+node bin/chrome2i18next.js -i locales -o i18next_locales
+
 if [[ -z $(git status --porcelain=v1) ]];
 then
-	echo \"Starting test procedure\";
+	echo \"chrome2i18next complete. Checking i18next2chrome…\";
 else
-	echo \"Please commit all changes\";
+	echo \"Please commit all changes before checking i18next2chrome\";
 	exit 1;
 fi
 
-node bin/chrome2i18next.js -i locales -o i18next_locales
 node bin/i18next2chrome.js -i i18next_locales -o locales --i18next_format json
